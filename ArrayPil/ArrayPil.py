994c0fd1e1c9baeaa3203ec7b1c6920dd2df1ec5
Daniil-7 <yas66yelchili@gmail.com>
2022-03-22 07:40:16 +0300

    us black lib

--- ArrayPil/ArrayPil.py
+++ ArrayPil/ArrayPil.py
@@ -2,36 +2,35 @@ from PIL import Image
 import numpy as np
 
 
-
-def ConvImgArray(imgdir, conv='RGB'):
-		im1 = np.asarray(Image.open(imgdir).convert(conv))
-		return im1
-		
-		
-def ImgSave(imgarray, dirsave, conv='RGB'):
-	   	im2 = Image.fromarray(imgarray, conv)
-	   	im2.save(dirsave)
+def ConvImgArray(imgdir, conv="RGB"):
+    im1 = np.asarray(Image.open(imgdir).convert(conv))
+    return im1
 
 
-def ConvPilArray(imgpil, conv='RGB'):
-		im3 = np.asarray(imgpil.convert(conv))
-		return im3
+def ImgSave(imgarray, dirsave, conv="RGB"):
+    im2 = Image.fromarray(imgarray, conv)
+    im2.save(dirsave)
 
 
-def ConvImgPil(imgarray, conv='RGB'):
-		im4 = Image.fromarray(imgarray, conv)
-		return im4
-		
-		
-#testing
+def ConvPilArray(imgpil, conv="RGB"):
+    im3 = np.asarray(imgpil.convert(conv))
+    return im3
+
+
+def ConvImgPil(imgarray, conv="RGB"):
+    im4 = Image.fromarray(imgarray, conv)
+    return im4
+
+
+# testing
 #
-#a1 = ConvImgArray('image1.jpg')
-#print(a1)
+# a1 = ConvImgArray('image1.jpg')
+# print(a1)
 #
-#ImgSave(a1, 'res.jpg')
+# ImgSave(a1, 'res.jpg')
 #
-#a2 = ConvImgPil(a1)
-#print(a2)
+# a2 = ConvImgPil(a1)
+# print(a2)
 #
-#a3 = ConvPilArray(a2)
-#print(a3)
+# a3 = ConvPilArray(a2)
+# print(a3)
